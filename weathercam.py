aafdafae26ad51cbe925dd13112fe232440e0e19
Brenna Moore <brenna@brenna.dev>
2021-04-29 20:06:48 -0700

    use boolean vars correctly

--- weathercam.py
+++ weathercam.py
@@ -4,18 +4,18 @@ from picamera import PiCamera
 from ftplib import FTP
 
 parser = argparse.ArgumentParser(description='Use the attached camera to take a photo and upload it to wunderground')
-parser.add_argument('flipvertical', type=bool, help='Flip the image vertically')
-parser.add_argument('fliphorizontal', type=bool, help='Flip the image horizontally')
-parser.add_argument('deviceid', type=str, help='Your wunderground camera device id')
-parser.add_argument('uploadkey', type=str, help='Your wunderground upload key')
+parser.add_argument('--flipvertical', action='store_true', dest='vflip', help='Flip the image vertically')
+parser.add_argument('--fliphorizontal', action='store_true', dest='hflip', help='Flip the image horizontally')
+parser.add_argument('--deviceid', type=str, help='Your wunderground camera device id')
+parser.add_argument('--uploadkey', type=str, help='Your wunderground upload key')
 
 args = parser.parse_args()
 fileName = 'still.jpg'
 
 with PiCamera() as camera:
-    camera.vflip = args.flipvertical
-    camera.hflip = args.fliphorizonal
-    camera.resolution(1024, 768)
+    camera.vflip = args.vflip
+    camera.hflip = args.hflip
+    camera.resolution = (1024, 768)
     sleep(2)
     camera.capture(fileName)
     pass
